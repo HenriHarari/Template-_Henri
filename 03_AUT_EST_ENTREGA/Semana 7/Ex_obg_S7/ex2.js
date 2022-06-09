c7cf07c91b8e6c4cc0636e58c5eecf2f1f678b72
HenriHarari <99759369+HenriHarari@users.noreply.github.com>
2022-06-09 14:38:34 -0300

    Semana 7

---
+++ 03_AUT_EST_ENTREGA/Semana 7/Ex_obg_S7/ex2.js
@@ -0,0 +1,7 @@
+
+function change() {
+    var nome1 = document.getElementById("1").value
+    var nome2 = document.getElementById("2").value
+    document.getElementById("1").value= nome2
+    document.getElementById("2").value= nome1
+}
